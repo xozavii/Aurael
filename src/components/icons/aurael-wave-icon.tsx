c65312f6c21c73d25debd53ef7a6888916af1785
Zxllwork <zxllwork@gmail.com>
2025-11-26 20:14:17 +0000

    <svg width="300" height="200" viewBox="0 0 300 200"      fill="none" st

--- src/components/icons/aurael-wave-icon.tsx
+++ src/components/icons/aurael-wave-icon.tsx
@@ -3,22 +3,17 @@ import React from "react";
 
 export const AuraelWaveIcon = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
     <svg
-        viewBox="0 0 120 120"
+        viewBox="0 0 300 200"
         fill="none"
         stroke="currentColor"
-        strokeWidth="5"
+        strokeWidth="12"
         strokeLinecap="round"
         strokeLinejoin="round"
         className={cn("w-6 h-6", className)}
         {...props}
     >
-      {/* Main curling wave */}
-      <path d="M10 70 C35 25, 85 25, 95 55 C100 70, 90 82, 75 78 C60 74, 60 55, 75 50" />
-      
-      {/* Extra small curl detail */}
-      <path d="M60 78 C70 70, 80 70, 90 75" />
-      
-      {/* Bottom wavy water line */}
-      <path d="M10 95 C20 90, 30 100, 40 95 C50 90, 60 100, 70 95 C80 90, 90 100, 110 95" />
+      <path d="M20 110 C80 40, 170 40, 200 90 C215 115, 190 130, 165 120 C135 110, 140 70, 185 65" />
+      <path d="M120 120 C150 110, 180 130, 220 120" />
+      <path d="M20 160 C50 150, 80 170, 110 160 C140 150, 170 170, 200 160 C230 150, 260 170, 280 160" />
     </svg>
 );
