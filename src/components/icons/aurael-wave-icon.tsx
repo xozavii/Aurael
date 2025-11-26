6caf290ac4f91f38e447f7b01c411926d9aa8229
Zxllwork <zxllwork@gmail.com>
2025-11-26 20:11:10 +0000

    <svg width="150" height="150" viewBox="0 0 100 100" fill="none" stroke="

--- src/components/icons/aurael-wave-icon.tsx
+++ src/components/icons/aurael-wave-icon.tsx
@@ -3,18 +3,16 @@ import React from "react";
 
 export const AuraelWaveIcon = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
+        viewBox="0 0 100 100"
         fill="none"
         stroke="currentColor"
-        strokeWidth="2"
+        strokeWidth="4"
         strokeLinecap="round"
-        strokeLinejoin="round"
         className={cn("w-6 h-6", className)}
         {...props}
     >
-        <path d="M4 8c4-2 8 2 12 0s8 2 8 2" stroke="#ff4f7b" />
-        <path d="M4 13c4-2 8 2 12 0s8 2 8 2" stroke="#ff4f7b" />
-        <path d="M4 18c4-2 8 2 12 0s8 2 8 2" stroke="#ff4f7b" />
+      <path d="M10 50 C30 20, 60 20, 70 40 C80 55, 70 65, 60 60" />
+      <path d="M40 60 C50 50, 60 50, 75 55" />
+      <path d="M10 75 C20 70, 30 80, 40 75 C50 70, 60 80, 70 75 C80 70, 90 80, 95 75" />
     </svg>
 );
