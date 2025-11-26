9207ae0e0f28e171c8af84f93c8f719c7340f34c
Zxllwork <zxllwork@gmail.com>
2025-11-26 16:39:02 +0000

    this one

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -41,7 +41,7 @@ export default function AppSidebar() {
         <SidebarHeader className="p-4">
             <Link href="/" className="flex items-center gap-2 group-data-[collapsible=icon]:justify-center">
                 <Sparkles className="w-8 h-8 text-primary group-data-[collapsible=icon]:w-6 group-data-[collapsible=icon]:h-6 transition-all" />
-                <h1 className="text-4xl font-headline font-bold text-primary-foreground group-data-[collapsible=icon]:hidden">Aurael</h1>
+                <h1 className="text-4xl font-headline font-bold text-primary group-data-[collapsible=icon]:hidden">Aurael</h1>
             </Link>
         </SidebarHeader>
         <SidebarContent className="p-2">
